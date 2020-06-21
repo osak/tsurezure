124adb1b65f8dc0e154e2f1b15fee317729a729d
Osamu Koga <osak.63@gmail.com>
2020-06-21 02:29:21 +0000

    Add title

--- web/index.tsx
+++ web/index.tsx
@@ -4,9 +4,16 @@ import * as ReactDOM from 'react-dom';
 import { Router } from '@reach/router';
 import { Main } from './page/main/Main';
 
+function Tsurezure() {
+    return <div className="main">
+        <h1 id="title">徒然</h1>
+        <Router>
+            <Main path="/" />
+        </Router>
+    </div>;
+}
+
 ReactDOM.render(
-    <Router>
-        <Main path="/" />
-    </Router>,
+    <Tsurezure />,
     document.getElementById('main')
 );
