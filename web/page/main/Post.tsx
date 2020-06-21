cae5295d554a0410aaa2cc25226962adcc009971
Osamu Koga <osak.63@gmail.com>
2020-06-21 02:36:43 +0000

    Pretty format date

--- web/page/main/Post.tsx
+++ web/page/main/Post.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import moment = require('moment');
 import { Model } from '../../model';
 
 export type Props = {
@@ -9,9 +10,10 @@ export function Post(props: Props) {
     const post = props.post;
 
     const anchor = `post-${post.id}`;
+    const postedAt = moment(post.posted_at).format('YYYY-MM-DD HH:mm:ssZ');
     return <article className="article">
         <header className="header"><a id={anchor} href={`#${anchor}`}>■</a></header>
         <section className="body">{post.body}</section>
-        <div className="posted-at">{post.posted_at}</div>
+        <div className="posted-at">{postedAt}</div>
     </article>;
 }
