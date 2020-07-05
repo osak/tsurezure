d577d4c12857e8705b1642df271a937022327fc4
Osamu Koga <osak.63@gmail.com>
2020-07-05 09:34:44 +0000

    admin bundle route

--- src/main.rs
+++ src/main.rs
@@ -130,9 +130,25 @@ async fn default_route(req: HttpRequest) -> Result<HttpResponse, std::io::Error>
     use actix_web::http::header::{HeaderName, HeaderValue};
 
     match req.path() {
-        "/bundle.js" => fs::NamedFile::open("web-dist/bundle.js").map(|n| n.into_response(&req).unwrap()),
+        "/index.bundle.js" => fs::NamedFile::open("web-dist/index.bundle.js").map(|n| n.into_response(&req).unwrap()),
         "/style.css" => fs::NamedFile::open("web-dist/style.css").map(|n| n.into_response(&req).unwrap()),
         _ => fs::NamedFile::open("web-dist/index.html")
+              .map(|n| {
+                  let mut resp = n.into_response(&req).unwrap();
+                  let headers = resp.headers_mut();
+                  headers.append(HeaderName::from_static("cache-control"), HeaderValue::from_static("no-cache"));
+                  resp
+              })
+    }
+}
+
+async fn default_admin_route(req: HttpRequest) -> Result<HttpResponse, std::io::Error> {
+    use actix_web::http::header::{HeaderName, HeaderValue};
+
+    match req.path() {
+        "/admin.bundle.js" => fs::NamedFile::open("web-dist/admin.bundle.js").map(|n| n.into_response(&req).unwrap()),
+        "/style.css" => fs::NamedFile::open("web-dist/style.css").map(|n| n.into_response(&req).unwrap()),
+        _ => fs::NamedFile::open("web-dist/admin.html")
               .map(|n| {
                   let mut resp = n.into_response(&req).unwrap();
                   let headers = resp.headers_mut();
@@ -196,7 +212,8 @@ async fn main() -> std::io::Result<()> {
                 .service(login))
             .service(web::scope("/admin")
                 .service(admin_get_post)
-                .service(create_post))
+                .service(create_post)
+                .default_service(web::resource("").route(web::get().to(default_admin_route))))
             .default_service(web::resource("").route(web::get().to(default_route)))
     })
     .bind(format!("0.0.0.0:{}", port))?
