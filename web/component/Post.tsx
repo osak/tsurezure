59c006da0dda386c25d74a15eea79eced8275342
Osamu Koga <osak.63@gmail.com>
2020-07-05 10:41:42 +0000

    Show updated time

--- web/component/Post.tsx
+++ web/component/Post.tsx
@@ -11,9 +11,11 @@ export function Post(props: Props) {
     const post = props.post;
 
     const postedAt = moment(post.posted_at).format('YYYY-MM-DD HH:mm:ssZ');
+    const updatedAt = post.updated_at && moment(post.updated_at).format('YYYY-MM-DD HH:mm:ssZ');
     return <article className="article">
         <header className="header"><Link to={`/posts/${post.id}`}>■</Link></header>
         <section className="body" dangerouslySetInnerHTML={{__html: post.body}} />
+        {updatedAt && <div className="updated-at">Updated: {updatedAt}</div>}
         <div className="posted-at">{postedAt}</div>
     </article>;
 }
--- web/model.ts
+++ web/model.ts
@@ -2,6 +2,7 @@ export namespace Model {
     export type Post = {
         id: number,
         body: string,
-        posted_at: Date
+        posted_at: Date,
+        updated_at: Date | null,
     };
 }
