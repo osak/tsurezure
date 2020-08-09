b1d9e29fefee98081af3eac70b2d7500d433809e
Osamu Koga <osak.63@gmail.com>
2020-08-09 22:12:43 +0000

    Link title to toppage

--- web/index.tsx
+++ web/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { Router } from '@reach/router';
+import { Router, Link } from '@reach/router';
 import { Main } from './page/main/Main';
 import { Single } from './page/single/Single';
 
 function Tsurezure() {
     return <div className="main">
-        <h1 id="title">徒然</h1>
+        <h1 id="title"><Link to="/" className="title--link">徒然</Link></h1>
         <Router>
             <Main path="/" />
             <Single path="/posts/:id" />
